e65762c7139c45cf2d06ba3e3067834c81ea81bd
Simon Boehm <simon@siboehm.com>
2021-05-28 16:14:21 +0200

    Refactor optimization & compilation code

--- lleaves/lleaves.py
+++ lleaves/lleaves.py
@@ -11,11 +11,13 @@ from lleaves.tree_compiler.objective_funcs import get_objective_func
 class Model:
     # machine-targeted compiler & exec engine
     _execution_engine = None
-    # IR representation of model
-    _ir_module = None
-    # compiled representation of IR
-    _compiled_module = None
-    compiled = False
+
+    # IR representation of model, as it comes unoptimized from the frontend
+    _IR_module_frontend: llvm.ir.Module = None
+    # IR Module, optimized by llvmlite
+    _IR_module: llvm.ModuleRef = None
+
+    # prediction function
     _c_entry_func = None
 
     def __init__(self, model_file=None):
@@ -32,55 +34,61 @@ class Model:
         """number of features"""
         return self._general_info["max_feature_idx"] + 1
 
-    @property
-    def ir_module(self):
-        if not self._ir_module:
-            self._ir_module = ir_from_model_file(self.model_file)
-        return self._ir_module
+    def _get_ir_from_frontend(self):
+        if not self._IR_module_frontend:
+            self._IR_module_frontend = ir_from_model_file(self.model_file)
+        return self._IR_module_frontend
 
-    @property
-    def execution_engine(self):
+    def _get_execution_engine(self):
         """
         Create an ExecutionEngine suitable for JIT code generation on
         the host CPU. The engine is reusable for an arbitrary number of
         modules.
         """
-        if not self._execution_engine:
-            llvm.initialize()
-            llvm.initialize_native_target()
-            llvm.initialize_native_asmprinter()
+        if self._execution_engine:
+            return self._execution_engine
 
-            # Create a target machine representing the host
-            target = llvm.Target.from_default_triple()
-            target_machine = target.create_target_machine()
-            # And an execution engine with an empty backing module
-            backing_mod = llvm.parse_assembly("")
-            self._execution_engine = llvm.create_mcjit_compiler(
-                backing_mod, target_machine
-            )
+        llvm.initialize()
+        llvm.initialize_native_target()
+        llvm.initialize_native_asmprinter()
+
+        # Create a target machine representing the host
+        target = llvm.Target.from_default_triple()
+        target_machine = target.create_target_machine()
+        # And an execution engine with an empty backing module
+        backing_mod = llvm.parse_assembly("")
+        self._execution_engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
         return self._execution_engine
+
+    def _get_optimized_module(self):
+        if self._IR_module:
+            return self._IR_module
+
+        # Create a LLVM module object from the IR
+        module = llvm.parse_assembly(str(self._get_ir_from_frontend()))
+        module.verify()
+
+        self._IR_module = module
+        return self._IR_module
 
     def compile(self):
         """
         Generate the LLVM IR for this model and compile it to ASM
         This function can be called multiple time, but will only compile once.
         """
-        if self._compiled_module:
+        if self._c_entry_func:
             return
 
-        # Create a LLVM module object from the IR
-        module = llvm.parse_assembly(str(self.ir_module))
-        module.verify()
-
         # add module and make sure it is ready for execution
-        self.execution_engine.add_module(module)
-        self.execution_engine.finalize_object()
-        self.execution_engine.run_static_constructors()
-        self._compiled_module = module
+        exec_engine = self._get_execution_engine()
+        exec_engine.add_module(self._get_optimized_module())
+        # run codegen
+        exec_engine.finalize_object()
+        exec_engine.run_static_constructors()
 
         # construct entry func
-        addr = self._execution_engine.get_function_address("forest_root")
-        # Return Value, pointer to 1D data, n_preds, pointer to alloced results arr
+        addr = exec_engine.get_function_address("forest_root")
+        # CFUNCTYPE params: void return, pointer to data, n_preds, pointer to results arr
         self._c_entry_func = CFUNCTYPE(
             None, POINTER(c_double), c_int, POINTER(c_double)
         )(addr)
