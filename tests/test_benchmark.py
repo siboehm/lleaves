48bf5c512d42cbe7bd8affd39e1b1ca2f1d47ad9
Simon Boehm <simon@siboehm.com>
2021-05-30 11:30:02 +0200

    Fix CI

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,12 +1,16 @@
+import os
+
 import lightgbm
 import numpy as np
 import pandas as pd
+import pytest
 
 import lleaves
 from benchmarks.simple_timeit import NYC_used_columns
 from benchmarks.train_NYC_model import feature_enginering
 
 
+@pytest.mark.skipif(os.environ["CI"], "We don't want to download the datasets on CI")
 def test_benchmark_datsets_correct_output():
     model_file_NYC = "tests/models/NYC_taxi/model.txt"
     model_file_airline = "tests/models/airline/model.txt"
