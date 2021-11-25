890baf5916a18bf207ffdc4c51d4584654af6e57
Simon Boehm <simon@siboehm.com>
2021-11-25 16:38:11 +0100

    A single-row prediction benchmark

--- benchmarks/c_bench/c_bench.cpp
+++ benchmarks/c_bench/c_bench.cpp
@@ -5,23 +5,46 @@
 #include <cstdlib>
 #include <iostream>
 
-static void bm_lleaves(benchmark::State& state) {
-  char *model_name = std::getenv("LLEAVES_BENCHMARK_MODEL");
+static void prediction_batch(benchmark::State &state) {
+    char *model_name = std::getenv("LLEAVES_BENCHMARK_MODEL");
 
-  std::ostringstream model_stream;
-  model_stream << "../../data/" << model_name << ".npy";
-  std::string model_file = model_stream.str();
-  cnpy::NpyArray arr = cnpy::npy_load(model_file);
+    std::ostringstream model_stream;
+    model_stream << "../../data/" << model_name << ".npy";
+    std::string model_file = model_stream.str();
+    cnpy::NpyArray arr = cnpy::npy_load(model_file);
 
-  auto *loaded_data = arr.data<double>();
-  ulong n_preds = arr.shape[0];
-  auto *out = (double *)(malloc(n_preds * sizeof(double)));
+    auto *loaded_data = arr.data<double>();
+    ulong n_preds = arr.shape[0];
+    auto *out = (double *) (malloc(n_preds * sizeof(double)));
 
-  for (auto _ : state){
-      // predict over the whole input array
-      forest_root(loaded_data, out, (int)0, (int)n_preds);
-  }
+    for (auto _: state) {
+        // batch-prediction over the whole input array
+        forest_root(loaded_data, out, (int) 0, (int) n_preds);
+        benchmark::DoNotOptimize(out);
+    }
 }
 
-BENCHMARK(bm_lleaves);
+BENCHMARK(prediction_batch);
+
+static void prediction_single_row(benchmark::State &state) {
+    char *model_name = std::getenv("LLEAVES_BENCHMARK_MODEL");
+
+    std::ostringstream model_stream;
+    model_stream << "../../data/" << model_name << ".npy";
+    std::string model_file = model_stream.str();
+    cnpy::NpyArray arr = cnpy::npy_load(model_file);
+
+    auto *loaded_data = arr.data<double>();
+    int n_preds = (int) arr.shape[0];
+    auto *out = (double *) (malloc(n_preds * sizeof(double)));
+    int i = 0;
+    for (auto _: state) {
+        // batch-prediction over the whole input array
+        forest_root(loaded_data, out, 0, 1);
+        benchmark::DoNotOptimize(out);
+        i = (i + 1) % n_preds;
+    }
+}
+
+BENCHMARK(prediction_single_row);
 BENCHMARK_MAIN();
