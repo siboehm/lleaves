4f7996617081143af0e7a4ff2087d726d85f37d0
Simon Boehm <simon@siboehm.com>
2025-10-05 18:39:58 -0700

    Fix unzip to overwrite without prompting
    Add -o flag to avoid interactive prompt that could hang CI.

--- benchmarks/data/setup_data.sh
+++ benchmarks/data/setup_data.sh
@@ -4,6 +4,6 @@ set -euox pipefail
 
 pushd benchmarks/data/
 wget -q https://f003.backblazeb2.com/file/lleaves-benchmark/benchmark_data.zip
-unzip benchmark_data.zip
+unzip -o benchmark_data.zip
 PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}$(pwd)/../.." python gen_npy.py
 popd
